e8f392b900c31d3f5ed23af99a52f2819bc9dd63
icekingoz <ozzyqa@gmail.com>
2026-02-26 19:46:27 -0800

    Test Commit

---
+++ src/main/java/org/demo/pages/Base.java
@@ -0,0 +1,38 @@
+package org.demo.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
+public abstract class Base {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    @BeforeClass
+    public void beforeClass(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    public void goToPage() {
+        driver.get("https://nop-qa.portnov.com/");
+    }
+
+    public void goToPage(String url) {
+        driver.get(url);
+    }
+
+    @AfterClass
+    public void afterClass(){
+        driver.quit();
+    }
+
+
+}
