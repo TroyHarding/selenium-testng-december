7b91b1c631fd705defa20bd518052be97ccbd3b8
Artem Oganesyan <56502558+ArtemOganesyan@users.noreply.github.com>
2026-03-05 16:26:39 -0800

    Refactor addToCart test for improved readability

--- src/test/java/tests/AddtoCartTests.java
+++ src/test/java/tests/AddtoCartTests.java
@@ -1,10 +1,14 @@
 package tests;
 
-import pages.BYOCPage;
 import base.Base;
-import pages.CartPage;
-import pages.HomePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import pages.BYOCPage;
+import pages.CartPage;
+
+import java.time.Duration;
 
 import static org.testng.Assert.assertTrue;
 
@@ -14,36 +18,31 @@ public class AddtoCartTests extends Base {
     public void addToCart() {
         goToPage();
         logger.info("I AM LOG!!!!");
-    
+
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-    
+
         By computersTopMenu = By.xpath("//ul[contains(@class,'top-menu')]//a[normalize-space()='Computers']");
         wait.until(ExpectedConditions.elementToBeClickable(computersTopMenu)).click();
-    
+
         wait.until(ExpectedConditions.urlContains("/computers"));
-    
+
         By desktopsSubCategory = By.xpath("//h2[@class='title']/a[normalize-space()='Desktops']");
         wait.until(ExpectedConditions.elementToBeClickable(desktopsSubCategory)).click();
-    
+
         By byocProduct = By.xpath("//h2[@class='product-title']/a[normalize-space()='Build your own computer']");
         wait.until(ExpectedConditions.elementToBeClickable(byocProduct)).click();
-    
+
         BYOCPage byocPage = new BYOCPage(driver);
         byocPage.byoc();
-    
-        By shoppingCartLink = By.xpath("//a[contains(@href,'/cart') and (normalize-space()='Shopping cart' or @class='ico-cart')]");
+
+        By shoppingCartLink = By.xpath("//a[contains(@href,'/cart') and (contains(normalize-space(),'Shopping cart') or contains(@class,'ico-cart'))]");
         wait.until(ExpectedConditions.elementToBeClickable(shoppingCartLink)).click();
-    
+
         CartPage cartPage = new CartPage(driver);
-    
+
         assertTrue(
-            cartPage.getCartProductInfo("Build your own computer").contains("Build your own computer"),
-            "Error"
+                cartPage.getCartProductInfo("Build your own computer").contains("Build your own computer"),
+                "Error"
         );
     }
-
-//        Optional Create at test to Update the Cart/
-//        Create Test to add 2 items, and delete one
-
-
 }
